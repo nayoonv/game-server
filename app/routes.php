c81a722256f27283607e410d0b0f4a1b1f1e3040
nayoonv <nykimv@com2us.com>
2022-02-17 18:46:22 +0900

    keep making upload-page

--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,7 @@ use App\Application\Actions\User\ViewUserAction;
 use App\Application\Actions\File\UploadFileAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\UploadedFileInterface as UploadedFile;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
@@ -32,20 +33,39 @@ return function (App $app) {
     });
 
     $app->post('/upload', function (Request $request, Response $response) {
-        $container = $app->getContainer();
-        $container['upload_directory'] = require __DIR__ . '/uploads';
-        $directory = $this->get('upload_directory');
 
-        $uploadedFiles = $request->getUploadedFiles();
+        $files = $request->getUploadedFiles();
+        $uploadedFile = $files['file'];
 
-        // handle single input with single file upload
-        $uploadedFile = $uploadedFiles['file'];
         if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
-            $filename = moveUploadedFile($directory, $uploadedFile);
-            $response->write('uploaded ' . $filename . '<br/>');
+            $filename = moveUploadedFile(__DIR__ . '/uploads', $uploadedFile);
+            $response->getBody()->write('Uploaded: ' . $filename . '<br/>');
         }
+
+        return $response;
     });
 
+    /**
+     * Moves the uploaded file to the upload directory and assigns it a unique name
+     * to avoid overwriting an existing uploaded file.
+     *
+     * @param string $directory The directory to which the file is moved
+     * @param UploadedFileInterface $uploadedFile The file uploaded file to move
+     *
+     * @return string The filename of moved file
+     */
+    function moveUploadedFile(string $directory, UploadedFile $uploadedFile)
+    {
+        $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+
+        // see http://php.net/manual/en/function.random-bytes.php
+        $basename = bin2hex(random_bytes(8));
+        $filename = sprintf('%s.%0.8s', $basename, $extension);
+
+        $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+
+        return $filename;
+    }
     $app->get('/file-upload', UploadFileAction::class);
 
     $app->post('/bye', function (Request $request, Response $response) {
@@ -62,13 +82,3 @@ return function (App $app) {
     });
 };
 
-function moveUploadedFile($directory, UploadedFile $uploadedFile)
-{
-    $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
-    $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
-    $filename = sprintf('%s.%0.8s', $basename, $extension);
-
-    $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
-
-    return $filename;
-}
